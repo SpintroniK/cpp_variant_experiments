4ea323a59911792b96432310ab155272447a478b
Jeremy Oden <jerem.oden@gmail.com>
2021-12-15 20:43:17 +0100

    Add rose_tree code.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# cpp_variant_experiments
+# C++ Variant Experiments
+
+Attempts to make a variant visitor for trees.
+
+## Rose Tree - Composable C++
+
+The first experiment is based on Ben Deane's talk at CppCon 2021: 
+[Composable C++: Principles and Patterns - Ben Deane - CppCon 2021](https://www.youtube.com/watch?v=1-IuTLrwpuU)
+
+A little had to be used to make it compile with GCC 10.
+
+The C++ file for that experiment is `rose_tree.cpp`.
---
+++ rose_tree.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <numeric>
+#include <string_view>
+#include <variant>
+#include <vector>
+
+template <typename ... F>
+class overloaded : private F...
+{
+public:
+  overloaded(F... f) : F(f)... {}
+  using F::operator()...;
+};
+
+
+template<typename T>
+struct rose_tree: std::variant<T, std::vector<rose_tree<T>>> 
+{
+    using base = typename rose_tree::variant;
+
+    template<typename U>
+    rose_tree(U&& v): std::variant<T, std::vector<rose_tree<T>>>{std::forward<U>(v)} 
+    {
+    }
+};
+
+template <typename T, typename TInit, typename FBranch, typename FLeaf>
+auto reduce_tree(const rose_tree<T>& t, TInit i, FBranch r_op, FLeaf t_op)
+{
+    return std::visit(overloaded
+    {
+        [&] (const T& leaf) { return t_op(leaf); },
+        [&] (const auto& branch) 
+        {
+            return std::transform_reduce(std::begin(branch), std::end(branch), i, r_op, [&] (const auto& t) { return reduce_tree(t, i, r_op, t_op); });
+        }
+    }, 
+    static_cast<rose_tree<T>::base>(t));
+}
+
+auto total_length(const rose_tree<std::string_view>& t) -> int
+{
+    return reduce_tree(t, 0, std::plus{}, [](const auto& leaf) -> int { return std::size(leaf); });
+}
+
+int main(int, char**) 
+{
+
+    rose_tree<std::string_view> node0{"3"};
+    std::vector<rose_tree<std::string_view>> node1{node0, "2"};
+    std::vector<rose_tree<std::string_view>> node2{"0", "1"};
+    std::vector<rose_tree<std::string_view>> node3{node2, node1};
+    
+    std::cout << "Total length = " << total_length(node3) << std::endl;
+
+    return 0;
+}
