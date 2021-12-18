00be9036c6c7a311712a4de5fd6628da45c0ca97
Jeremy Oden <jerem.oden@gmail.com>
2021-12-18 21:21:57 +0100

    Add reduce rose tree.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # C++ Variant Experiments
 
 Attempts to make a variant visitor for trees.
+Below are the different steps that led me to a solution that, I believe, is acceptable.
 
 ## Rose Tree - Composable C++
 
@@ -36,3 +37,16 @@ This one is a generalization of the previous one (Shared Pointer Rose Tree). It 
 The code gets more complicated, but it also gets very generic! It would be great to get rid of those `shared_ptr`, though.
 
 As always, tested with GCC >= 10, and here's the compiler explorer link: [https://godbolt.org/z/a3x5Ph77P](https://godbolt.org/z/a3x5Ph77P).
+
+## Transform & Reduce a Rose Tree
+
+This one gets rid of the `shared_ptr` trick, and pretty much achieves what Ben Deane describes at [the end of his CppCon talk](https://www.youtube.com/watch?v=1-IuTLrwpuU). It allows an arbitrary number of types for the nodes.  
+The `reduce_tree` function is used to apply a transform-reduce algorithm to the rose tree.
+
+The tree I used in the code is taken from [this article](https://blog.ploeh.dk/2019/08/05/rose-tree-catamorphism/). It shows that the tree can be reduced to a single number, even though its nodes and leaves have two different types: integers and strings, respectively.
+
+Everything is generic until the tree is actually defined as a tree of strings and integers. When the `reduce_tree` function is applied to the tree, an initial value of `0` is passed as the second argument, then, the "reduce" function is a lambda that returns the sum of two elements.
+The transform function, on the other hand, uses the `overloaded` idiom to handle the two nodes types separately: if the node is a string, its length is returned, if it's an integer, that same integer is returned.
+
+As always, the code has been tested with GCC >= 10.
+And here's the compiler explorer link: [https://godbolt.org/z/1qhjWx33P](https://godbolt.org/z/1qhjWx33P).
---
+++ reduce_rose_tree.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <variant>
+#include <vector>
+
+
+template <typename ... F>
+class overloaded : private F...
+{
+public:
+    overloaded(F... f) : F(f)... {}
+    using F::operator()...;
+};
+
+struct nil{};
+
+template <typename... T>
+struct Tree : std::variant<nil, std::pair<std::variant<T...>, std::vector<Tree<T...>>>>
+{
+    using base = typename Tree::variant;
+
+    template<typename U>
+    Tree(U&& v) : std::variant<nil, std::pair<std::variant<T...>, std::vector<Tree<T...>>>>{std::forward<U>(v)} 
+    {
+    }
+};
+
+template <typename... T, typename TInit, typename FReduce, typename FTransform>
+auto reduce_tree(const Tree<T...>& t, TInit init, FReduce r_op, FTransform t_op)
+{
+    return std::visit(overloaded
+    {
+        [&] (nil) { return init; },
+        [&] (const auto& p)
+        {
+            return r_op(std::visit(t_op , p.first), 
+                        std::transform_reduce(std::begin(p.second), std::end(p.second), init, r_op, [&] (const auto& t) 
+                        { 
+                            return reduce_tree(t, init, r_op, t_op); 
+                        }));
+        }
+    }, 
+    static_cast<Tree<T...>::base>(t));
+}
+
+using Node = Tree<std::string, int>;
+using VariantType = std::variant<std::string, int>;
+
+template <typename T>
+Node MakeNode(const T& n, const std::vector<Node>& c = { nil{} })
+{
+    return Node{ std::make_pair(n, c) };
+}
+
+ 
+int main(int, char**) 
+{
+
+    using namespace std::string_literals;
+
+    // This is where I found that tree: https://blog.ploeh.dk/2019/08/05/rose-tree-catamorphism/
+    const auto tree = MakeNode(42, 
+    { 
+        MakeNode(1337, { MakeNode("foo"s), MakeNode("bar"s) }),
+        MakeNode(2112, 
+        { 
+            MakeNode(90125, { MakeNode("baz"s), MakeNode("qux"s), MakeNode("quux"s) }),
+            MakeNode("quuz"s)
+        }),
+        MakeNode("corge"s)
+    });
+
+
+    const auto r = reduce_tree(tree, 0, 
+                               [](auto i, auto j) { return i + j; },    // Reduce function
+                               overloaded                               // Transform "overloaded" function
+                               {
+                                    [] (const std::string& v) -> int { return std::size(v); },  // If the node is the string => return its length
+                                    [] (auto i) { return i; }                                   // If the node is an int => return its value
+                               });
+
+    std::cout << "Total = " << r << std::endl;
+
+    return 0;
+}
